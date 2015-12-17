b509388b50bf4aefb97470b956a4c681a590cd58
azhar <azhar.amirr@gmail.com>
2015-12-17 13:10:53 +0700

    updated nprogress

--- js/core.js
+++ js/core.js
@@ -1,23 +1,33 @@
 var posts;
-var port = chrome.extension.connect({name: "detikcon Newsfeed"});
-var init = function(){
 
-  document.$getJSON("http://detikcom.herokuapp.com/newsfeeds", function(posts) {
-    posts.reverse();
-    for (var i = 0;i < posts.length;i++){
-      addPost(posts[i]);
+document.addEventListener('DOMContentLoaded', function() {
+  NProgress.start();
+  initStart();
+});
+
+var initStart = function(){
+  posts = document.querySelector(".posts");
+  $.getJSON("http://detikcom.herokuapp.com/newsfeeds", function(posts) {
+    if (posts.success) {
+      var newsfeed = posts.data.nonheadline;
+      newsfeed.reverse();
+      for (var i = 0;i < newsfeed.length;i++){
+        addNewsFeed(newsfeed[i]);
+        if (i == 15){
+          setTimeout(function(){ NProgress.done()}, 2000)
+        }
+      }
     }
   });
-
 }
 
-var addPost = function(post) {
+var addNewsFeed = function(post) {
   var postDom = document.createElement("div");
   postDom.classList.add("post");
 
-  postDom.innerHTML += "<h2><a href='"+post.url+"' target='_blank'>"+post.title+"</h2>";
-  postDom.innerHTML += "<p>" + post.description + "</p>";
-  postDom.innerHTML += "<span class='avatar'> <img src='"+post.image_url+"'></span>";
+  postDom.innerHTML += "<h2><a href='"+post.article_url+"' target='_blank'>"+post.title+"</h2>";
+  postDom.innerHTML += "<p>" + post.resume + "</p>";
+  postDom.innerHTML += "<span class='avatar'> <img src='"+post.image+"?w=150'></span>";
 
   posts.insertBefore(postDom, posts.firstChild);
 }
